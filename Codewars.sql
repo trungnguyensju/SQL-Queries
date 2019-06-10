d983eac69819d7eb8583d335b53f6f45367cdec9
Trung Nguyen <trungn2015@gmail.com>
2019-06-10 09:19:56 -0700

    Add files via upload

--- Codewars.sql
+++ Codewars.sql
@@ -5,3 +5,15 @@ SELECT DATE(created_at) AS date,
 FROM posts
 GROUP BY DATE(created_at)
 
+--5 KYU Simple Hierarchical structure
+WITH RECURSIVE employee_levels (n) AS (
+    SELECT 1, id, first_name, last_name, manager_id
+    FROM employees
+    WHERE manager_id IS NULL
+    UNION ALL
+    SELECT n + 1, e.id, e.first_name, e.last_name, e.manager_id
+    FROM employees e JOIN employee_levels m ON e.manager_id = m.id     
+)
+
+SELECT n AS level, id, first_name, last_name, manager_id FROM employee_levels
+
